a03d59221467f1c3e1def0edfeb08e499c0bb6d6
Dean White <de4nwhit3@gmail.com>
2023-10-03 01:54:05 +0200

    added go router

---
+++ lib/2_application/core/routes.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:todo_app/2_application/pages/home/home_page.dart';
+import 'package:todo_app/2_application/pages/settings/settings_page.dart';
+
+import 'go_router_oberserver.dart';
+
+final GlobalKey<NavigatorState> _rootNavigatorKey =
+    GlobalKey<NavigatorState>(debugLabel: 'root');
+
+final GlobalKey<NavigatorState> _shellNavigatorKey =
+    GlobalKey<NavigatorState>(debugLabel: 'shell');
+
+final routes = GoRouter(
+  navigatorKey: _rootNavigatorKey,
+  initialLocation: '/dashboard',
+  observers: [GoRouterObserver()],
+  routes: [
+    GoRoute(
+      path: '/home/settings',
+      builder: (context, state) {
+        return const SettingsPage();
+      },
+    ),
+    ShellRoute(
+      navigatorKey: _shellNavigatorKey,
+      builder: (context, state, child) => child,
+      routes: [
+        GoRoute(
+          path: '/home/:tab',
+          builder: (context, state) => HomePage(
+            key: state.pageKey,
+            tab: state.pathParameters['tab']!,
+          ),
+        ),
+      ],
+    ),
+  ],
+);
